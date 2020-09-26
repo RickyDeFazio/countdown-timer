82d1131be8a6085dde84b4468768049b1db843e8
Ricky DeFazio <rickysdefazio@gmail.com>
2020-09-26 18:52:16 -0400

    Add vue.js

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
-const days = document.getElementById('days')
-const hours = document.getElementById('hours')
-const minutes = document.getElementById('minutes')
-const seconds = document.getElementById('seconds')
+var app = new Vue({
+  el: '#app',
+  data: {
+    days: '00',
+    hours: '00',
+    minutes: '00',
+    seconds: '00',
+    target: 'Jan 1 2021'
+  },
+  methods: {
+    countdown() {
+      const then = new Date(this.target)
+      const now = new Date();
+      let totalSeconds = (then - now) / 1000;
 
-const target = '31 Dec 2020';
+      const daysRemaining = Math.floor(totalSeconds / 3600 / 24);
+      const hoursRemaining = Math.floor(totalSeconds / 3600) % 24;
+      const minutesRemaining = Math.floor(totalSeconds / 60) % 60;
+      const secondsRemaining = Math.floor(totalSeconds) % 60;
 
-function countdown() {
-  const then = new Date(target)
-  const now = new Date();
-  let totalSeconds = (then - now) / 1000;
-
-  const daysRemaining = Math.floor(totalSeconds / 3600 / 24);
-  const hoursRemaining = Math.floor(totalSeconds / 3600) % 24;
-  const minutesRemaining = Math.floor(totalSeconds / 60) % 60;
-  const secondsRemaining = Math.floor(totalSeconds) % 60;
-
-  days.textContent = formatTime(daysRemaining);
-  hours.textContent = formatTime(hoursRemaining);
-  minutes.textContent = formatTime(minutesRemaining);
-  seconds.textContent = formatTime(secondsRemaining);
-}
-
-function formatTime(time) {
-  return time < 10 ? `0${time}` : time;
-}
-
-setInterval(countdown, 1000);
+      this.days = this.formatTime(daysRemaining);
+      this.hours = this.formatTime(hoursRemaining);
+      this.minutes = this.formatTime(minutesRemaining);
+      this.seconds = this.formatTime(secondsRemaining);
+    },
+    formatTime(time) {
+      return time < 10 ? `0${time}` : time;
+    }
+  },
+  mounted() {
+    setInterval(this.countdown, 1000);
+  }
+});
